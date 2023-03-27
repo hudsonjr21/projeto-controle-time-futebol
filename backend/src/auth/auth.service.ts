51e056634c77b908a6a311a18ffac6867769b660
Hudson de Jesus Ferreira Junior <hudson.junior@MATRIZ.GRUPOADRIANOCOBUCCIO.LOCAL>
2023-03-27 11:41:52 -0300

    implementacao de estrategia local e validacao

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,8 +1,28 @@
+import { UserService } from './../user/user.service';
 import { Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
+import { UnauthorizedError } from './erros/unauthorized.error';
 
 @Injectable()
 export class AuthService {
-    validateUser(email: string, password: string) {
-        throw new Error('Method not implemented.');
+    constructor(private readonly userService: UserService) {}
+
+    async validateUser(email: string, password: string) {
+        const user = await this.userService.findByEmail(email);
+
+        if (user) {
+            //Checar se a senha informada corresponde a hash que esta no banco.
+
+            const isPasswordValid = await bcrypt.compare(password, user.password);
+
+            if (isPasswordValid) {
+                return {
+                    ...user,
+                    password: undefined,
+                };
+            }
+        }
+        // se chegar aqui, significa que não encontrou um user e/ou a senha não corresponde.
+        throw new UnauthorizedError('Email address or password provided is incorrect.',);
     }
 }
