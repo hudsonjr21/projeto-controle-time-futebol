ec1497059ae6e4a82732a2979242166c1792d49c
Hudson de Jesus Ferreira Junior <hudson.junior@MATRIZ.GRUPOADRIANOCOBUCCIO.LOCAL>
2023-03-27 14:32:09 -0300

    tornado login publico para acesso e gerando token

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -2,11 +2,13 @@ import { AuthRequest } from './models/AuthRequest';
 import { AuthService } from './auth.service';
 import { Controller, HttpCode, HttpStatus, Post, Request, UseGuards } from '@nestjs/common';
 import { LocalAuthGuard } from './guards/local-auth.guard';
+import { IsPublic } from './decorators/is-public.decorator';
 
 @Controller()
 export class AuthController {
     constructor(private readonly authService: AuthService) {}
 
+    @IsPublic()
     @Post('login')
     @HttpCode(HttpStatus.OK)
     @UseGuards(LocalAuthGuard)
